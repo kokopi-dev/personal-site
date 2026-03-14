bd7ec0d121faf4390db4627e7527ef951010fb3f
kokopi-dev <km.commissions@gmail.com>
2026-03-15 02:43:19 +0900

    update:projects

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "Pulling latest code..."
 git pull
 
 echo "Building and restarting containers..."
-docker compose up -d --build --remove-orphans
+docker compose up -d --build
 
 echo "Cleaning up old images..."
 docker image prune -f
